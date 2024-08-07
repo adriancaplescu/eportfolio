67de4f6769731b9161430a7703169c35c0a2274e
Caplescu Adrian <adrian.g.caplescu@gmail.com>
2024-08-07 19:22:53 +0300

    Remove git submodules and add backend/frontend as regular directories

---
+++ backend/libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,60 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { AbstractEntity } from './abstract.entity';
+import {
+  EntityManager,
+  FindOptionsRelations,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+
+export abstract class AbstractRepository<T extends AbstractEntity<T>> {
+  protected abstract readonly logger: Logger;
+
+  constructor(
+    protected readonly entityRepository: Repository<T>,
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async create(entity: T): Promise<T> {
+    return this.entityManager.save(entity);
+  }
+
+  async findOne(
+    where: FindOptionsWhere<T>,
+    relations?: FindOptionsRelations<T>,
+  ): Promise<T> {
+    const document = await this.entityRepository.findOne({ where, relations });
+
+    if (!document) {
+      this.logger.warn('Document was not found with this filterQuery: ', where);
+      throw new NotFoundException('Document was not found');
+    }
+    return document;
+  }
+
+  async findOneAndUpdate(
+    where: FindOptionsWhere<T>,
+    partialEntity: QueryDeepPartialEntity<T>,
+  ): Promise<T> {
+    const updateResult = await this.entityRepository.update(
+      where,
+      partialEntity,
+    );
+
+    if (!updateResult.affected) {
+      this.logger.warn('Entity was not found with where ', where);
+      throw new NotFoundException('Entity was not found');
+    }
+
+    return this.findOne(where);
+  }
+
+  async find(where: FindOptionsWhere<T>): Promise<T[]> {
+    return this.entityRepository.findBy(where);
+  }
+
+  async findOneAndDelete(where: FindOptionsWhere<T>) {
+    await this.entityRepository.delete(where);
+  }
+}
