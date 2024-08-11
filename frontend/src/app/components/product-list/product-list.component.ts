e0806de8bae96832260c10c2477db487b7c98797
Caplescu Adrian <adrian.g.caplescu@gmail.com>
2024-08-11 15:37:45 +0300

    make images  smaler

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -9,11 +10,15 @@ import { ProductService } from '../../services/product.service';
 export class ProductListComponent {
   products: any[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit() {
     this.productService.getProducts().subscribe((data: any) => {
       this.products = data;
     });
+  }
+
+  selectedProduct(id: number) {
+    this.router.navigate(['/products', id]);
   }
 }
