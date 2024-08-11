f0f575d999853485310f254458fcaa454a4e4697
Caplescu Adrian <adrian.g.caplescu@gmail.com>
2024-08-11 14:09:34 +0300

    fix photo display

--- backend/apps/products/src/main.ts
+++ backend/apps/products/src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { ProductsModule } from './products.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ProductsModule } from './products.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(ProductsModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
-  app.useStaticAssets(join(__dirname, '..', 'uploads'));
+  const uploadsPath = join(process.cwd(), 'uploads');
+  app.use('/uploads', express.static(uploadsPath));
 
   await app.listen(3000);
 }
